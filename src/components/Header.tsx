223f723f9ae79ff9b313b7dec3c9128d7638d3ed
mohitmalviya0707 <mohitmalviya0107@gmail.com>
2025-09-29 18:46:40 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,13 +11,13 @@ import { useLanguage } from "@/contexts/LanguageContext";
 //   { code: "te", name: "Telugu", native: "తెలుగు" },
 //   { code: "or", name: "Odia", native: "ଓଡ଼ିଆ" }
 // ];
-  const languages = [
-    { code: 'hi' as Language, name: 'हिंदी', flag: '🇮🇳' },
-    { code: 'en' as Language, name: 'English', flag: '🇬🇧' },
-    { code: 'od' as Language, name: 'ଓଡ଼ିଆ', flag: '🇮🇳' },
-    { code: 'te' as Language, name: 'తెలుగు', flag: '🇮🇳' },
-    { code: 'bn' as Language, name: 'বাংলা', flag: '🇮🇳' },
-    { code: 'ta' as Language, name: 'தமிழ்', flag: '🇮🇳' }, { code: 'ml' as Language, name: 'മലയാളം', flag: '🇮🇳' }, { code: 'kn' as Language, name: 'ಕನ್ನಡ', flag: '🇮🇳' }, { code: 'gu' as Language, name: 'ગુજરાતી', flag: '🇮🇳' }, { code: 'mr' as Language, name: 'मराठी', flag: '🇮🇳' }, { code: 'pa' as Language, name: 'ਪੰਜਾਬੀ', flag: '🇮🇳' }, { code: 'as' as Language, name: 'অসমীয়া', flag: '🇮🇳' },
+const languages = [
+  { code: 'hi' as Language, name: 'हिंदी', flag: '🇮🇳' },
+  { code: 'en' as Language, name: 'English', flag: '🇬🇧' },
+  { code: 'od' as Language, name: 'ଓଡ଼ିଆ', flag: '🇮🇳' },
+  { code: 'te' as Language, name: 'తెలుగు', flag: '🇮🇳' },
+  { code: 'bn' as Language, name: 'বাংলা', flag: '🇮🇳' },
+  { code: 'ta' as Language, name: 'தமிழ்', flag: '🇮🇳' }, { code: 'ml' as Language, name: 'മലയാളം', flag: '🇮🇳' }, { code: 'kn' as Language, name: 'ಕನ್ನಡ', flag: '🇮🇳' }, { code: 'gu' as Language, name: 'ગુજરાતી', flag: '🇮🇳' }, { code: 'mr' as Language, name: 'मराठी', flag: '🇮🇳' }, { code: 'pa' as Language, name: 'ਪੰਜਾਬੀ', flag: '🇮🇳' }, { code: 'as' as Language, name: 'অসমীয়া', flag: '🇮🇳' },
     
   ];
 
